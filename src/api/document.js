369d190a8c69956d49fb0f1c19bf6025e7c40d14
Ivan Luetic <ivan.luetic10@gmail.com>
2025-08-23 03:14:40 +0200

    test

--- src/api/document.js
+++ src/api/document.js
@@ -3,42 +3,27 @@ import { useDocumentStore } from '@/stores/document'
 
 export const getDocuments = async () => {
   const documentStore = useDocumentStore()
-  /*   const response = await apiConfig.get('/document')
+  /* const response = await apiConfig.get('/document')
   const userDocuments = response.data.data
   documentStore.documents = userDocuments */
-  return
-}
-
-export const getDocumentById = async (documentId) => {
-  const response = await apiConfig.get('/document/:id', documentId)
-  const document = response.data.data
-  return document
-}
-
-export const signDocument = async (document) => {
-  const documentStore = useDocumentStore()
-  const response = await apiConfig.post('/workout/document/sign ', document)
-  const signedDocument = response.data.data
-  console.log('doc', signedDocument)
-
-  documentStore.documents.push(signedDocument)
-
-  // And if you also want to keep your test document
   const testDocument = {
     file: {
-      buffer: { type: 'Buffer', data: Array(103134) },
+      buffer: { type: 'Buffer', data: Array(1) },
       encoding: '7bit',
       fieldname: 'file',
       mimetype: 'image/jpeg',
-      originalname: 'SECOND_DOC.JPEG',
+      originalname: 'test_doc.jpeg',
       size: 103134,
       id: 3,
     },
   }
-
   documentStore.documents.push(testDocument)
-  console.log('docss', documentStore.documents)
+  return
+}
 
+export const signDocument = async (document) => {
+  const response = await apiConfig.post('/workout/document/sign ', document)
+  const signedDocument = response.data.data
   return signedDocument
 }
 
